4270a8433948fceaba591baa7aadfde4ef0b1a0b
alexshilov86 <avshilov995@gmail.com>
2025-09-24 22:11:29 +0300

    add help command

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     if not check_reg(message.from_user.id):
-        bot.send_message(message.from_user.id, "Для регистрации введите секретный код")
+        bot.send_message(message.from_user.id, "Вы не зарегистрированы. Для регистрации введите секретный код")
     else:
-        bot.send_message(message.from_user.id, "Вы зарегистрированы")
+        bot.send_message(message.from_user.id, "Внесите данные в формате ФИО; Проект; Часы; Комментарий. Например,\nСергей; 13031; 8; Выгрузка фур")
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    start_message(message)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -20,6 +24,7 @@ def get_text_messages(message):
         reg_ans = add_user_to_reg(message.from_user.id)        
         if reg_ans['success']:
             bot.send_message(message.from_user.id, "Регистрация прошла успешно")
+            bot.send_message(message.from_user.id, "Внесите данные в формате ФИО; Проект; Часы; Комментарий. Например,\nСергей; 13031; 8; Выгрузка фур")
         else:
             bot.send_message(message.from_user.id, "Не удалось зарегистрироваться")
 
