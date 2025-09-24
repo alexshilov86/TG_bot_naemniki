1a1e413acc736ebe60bca8b7747683b03df64066
alexshilov86 <avshilov995@gmail.com>
2025-09-24 21:41:05 +0300

    skhodnya_staff_bot

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import gspread
+import os, time
+from dotenv import load_dotenv
+from user_info import check_reg, add_user_to_reg
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    if not check_reg(message.from_user.id):
+        bot.send_message(message.from_user.id, "Для регистрации введите секретный код")
+    else:
+        bot.send_message(message.from_user.id, "Вы зарегистрированы")
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '5378':
+        reg_ans = add_user_to_reg(message.from_user.id)        
+        if reg_ans['success']:
+            bot.send_message(message.from_user.id, "Регистрация прошла успешно")
+        else:
+            bot.send_message(message.from_user.id, "Не удалось зарегистрироваться")
+
+
+if __name__=='__main__':
+    while True:
+        try:
+            bot.polling(non_stop=True, interval=0)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            continue
---
+++ user_info.py
@@ -0,0 +1,35 @@
+import json
+
+def check_reg (chat_id):
+    file_path = 'user_info_reg.json'
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            registered_id_list = data['registered_id_list']
+            if chat_id in registered_id_list:
+                return True
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except json.JSONDecodeError:
+        print(f"Error: Could not decode JSON from '{file_path}'. Check file format.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    return False
+
+def add_user_to_reg(chat_id):
+    file_path = 'user_info_reg.json'
+    reg_ans = {'success' : False}
+    try:
+        with open(file_path, 'r') as file:    
+            data = json.load(file)
+            registered_id_list = data['registered_id_list']
+        registered_id_list.append(chat_id)
+        with open(file_path, 'w') as file:
+            json.dump(registered_id_list, file, indent=4)
+        reg_ans['success'] = True
+    except Exception as e:
+        reg_ans['success'] = False
+    return reg_ans
+    
+
+print (check_reg('1234'))
